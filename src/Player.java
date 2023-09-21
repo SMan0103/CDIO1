7dd636fbb16f7f3f7a8224f1a393e60e8ae04999
Anders Greve Soerensen <anders1709@gmail.com>
2023-09-21 18:05:00 +0200

    Change names to follow camelCase naming convention

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,7 @@
 public class Player {
-        private String player_name;
+        private String playerName;
 
-        public void  player(String player_name) {
-            this.player_name = player_name;
+        public void  player(String playerName) {
+            this.playerName = playerName;
         }
 }
