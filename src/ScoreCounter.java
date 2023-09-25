540c5f3906442aec514a916376424de3d2353f59
Christian Lundquist <christian.lundquist@outlook.dk>
2023-09-25 14:43:28 +0200

    Added score to player class

---
+++ src/ScoreCounter.java
@@ -0,0 +1,29 @@
+public class ScoreCounter {
+    public static void Score(){
+        
+        Player player1; 
+        Player player2;
+        int playerscore1 = 0 ;
+        int playerscore2 = 0 ;
+        int score = 0;
+        if (int i=0; i < score; i++) {
+            player1+= playerscore1;
+            System.out.println(player1+": Your score is " + playerscore1);
+        }
+        
+        if (int j = 0; j < playerscore2; j++) {
+            playerscore2+=;
+            System.out.println(player2+": Your score is " + playerscore2);
+        }
+             
+        if (playerscore1>=playerscore2) {
+            System.out.println(player1+" is the winner!");
+
+          }
+            else {
+                System.out.println(player2+ " is the winner!");
+            }
+        }
+             
+    }
+
