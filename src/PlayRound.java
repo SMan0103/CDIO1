dbb6c5ecba275116326f7de199a91683c87f5e52
Niklas Jensen <niklaspj2004@gmail.com>
2023-09-25 15:17:07 +0200

    VERY INCOMPLETE! round system and score counter!

--- src/PlayRound.java
+++ src/PlayRound.java
@@ -1,20 +1,28 @@
 public class PlayRound {
 
-    public static void playRound(Player player1, Player player2) {
+    public static boolean playRound(Player player1, Player player2) { // returns true if game is over
         Dice dice = new Dice(2, 6);
 
-        System.out.println("Player ones turn.");
-        var throwResultsPlayerOne = dice.DiceThrow();
-        System.out.println("Your throw was:");
-        System.out.println("Dice 1: " + throwResultsPlayerOne.individualResults[0]);
-        System.out.println("Dice 2: " + throwResultsPlayerOne.individualResults[1]);
-        System.out.println("The sum of your throw is: " + throwResultsPlayerOne.addedResult);
+        Player[] playerList = {player1, player2};
 
-        System.out.println("Player two's turn.");
-        var throwResultsPlayerTwo = dice.DiceThrow();
-        System.out.println("Your throw was:");
-        System.out.println("Dice 1: " + throwResultsPlayerTwo.individualResults[0]);
-        System.out.println("Dice 2: " + throwResultsPlayerTwo.individualResults[1]);
-        System.out.println("The sum of your throw is: " + throwResultsPlayerTwo.addedResult);
+        for (Player _player: playerList) {
+            System.out.println(_player.playerName + "'s turn.");
+            var throwResults = dice.DiceThrow();
+            System.out.println("Your throw was:");
+            System.out.println("Dice 1: " + throwResults.individualResults[0]);
+            System.out.println("Dice 2: " + throwResults.individualResults[1]);
+            System.out.println("The sum of your throw is: " + throwResults.addedResult);
+            _player.addScore(throwResults.addedResult);
+            if (hasWon(_player)) {
+                System.out.println(_player.playerName + " Has Won!");
+                return true; // terminate game
+            }
+        }
+        return false;
+    
+    }
+
+    private static boolean hasWon(Player player) {
+        return player.getScore() >= 40;
     }
 }
